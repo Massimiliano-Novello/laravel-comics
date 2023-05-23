027e0c8cc9614c9edea58030eabd1ed082959a8c
Massimiliano-Novello <massinov5@gmail.com>
2023-05-23 17:01:31 +0200

    card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app');
 
 @section('content')
+<section class="my_card py-4">
     <div class="container">
         <div class="row row-cols-6">
             @foreach ($books as $book)
                 <div class="col">
-                    <img src="{{ $book['thumb'] }}" alt="">
                     <div class="card">
+                    <img src="{{ $book['thumb'] }}" alt="">
                         <h3>{{ $book['series'] }}</h3>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
+</section>
 @endsection
 
